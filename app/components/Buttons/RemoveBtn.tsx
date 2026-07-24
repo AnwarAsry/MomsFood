6721858bc0f10ab07220194a50f5a46b9eb7d5a1
AnwarAsry <anwareducation03@gmail.com>
2026-07-24 19:14:15 +0200

    btn components

---
+++ app/components/Buttons/RemoveBtn.tsx
@@ -0,0 +1,18 @@
+interface IRemoveBtnProps {
+    onClick: () => void;
+    list: any[]
+}
+
+export const RemoveBtn = ({ onClick, list }: IRemoveBtnProps) => {
+    return (
+        <button
+            type="button"
+            onClick={onClick}
+            disabled={list.length === 1}
+            className={`${list.length === 1 ? "opacity-0" : "opacity-100"} ${list.length === 1 ? "cursor-not-allowed" : "cursor-pointer"} w-7 h-7 flex shrink-0 items-center justify-center rounded-lg border border-black/30 text-base`}
+            aria-label="Remove"
+        >
+            ×
+        </button>
+    );
+}
