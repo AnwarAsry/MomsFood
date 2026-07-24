f6ac7a156f01e15562fffb461ec22b7e5ff11975
AnwarAsry <anwareducation03@gmail.com>
2026-07-24 02:03:43 +0200

    route to the page

--- app/routes.ts
+++ app/routes.ts
@@ -3,5 +3,6 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
     index("routes/_index.tsx"),
     route("new", "./routes/NewEntry.tsx"),
+    route("about", "./routes/About.tsx"),
     route(":id", "./routes/_index.$id.tsx"),
 ] satisfies RouteConfig;
