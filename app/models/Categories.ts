f7c6b3e948c6eb62f50f708668bb2a028dae0e6f
AnwarAsry <anwareducation03@gmail.com>
2026-07-24 12:41:52 +0200

    check mark and category model

---
+++ app/models/Categories.ts
@@ -0,0 +1,9 @@
+export type Category =
+    | "Chicken"
+    | "Soup"
+    | "Meat"
+    | "Dessert"
+    | "Pasta"
+    ;
+
+export const Categories: Category[] = ["Chicken", "Soup", "Meat", "Dessert", "Pasta"];
