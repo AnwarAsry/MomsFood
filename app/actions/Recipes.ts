565b1bd9fc136e629885d0d5190949030c72723d
AnwarAsry <anwareducation03@gmail.com>
2026-07-25 12:16:24 +0200

    get all recipes

--- app/actions/Recipes.ts
+++ app/actions/Recipes.ts
@@ -4,7 +4,15 @@ import type { ServerActionResponse } from "~/models/response/ServerAction";
 // Create Recipe
 
 // Get all Recipes
+export const getAllRecipes = async (): Promise<ServerActionResponse<IRecipe[] | null>> => {
+    try {
+        // Method to the backend
 
+        return { message: "Recipes retrived!", success: true, data: [defaultRecipe] }
+    } catch (e) {
+        return { message: `FAILED TO FETCH RECIPES: ${e}`, success: false, data: null }
+    }
+}
 
 // Get Recipe
 export const getRecipeById = async (id: string): Promise<ServerActionResponse<IRecipe | null>> => {
