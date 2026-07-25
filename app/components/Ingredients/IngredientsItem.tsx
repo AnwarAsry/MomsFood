3fcffd50e2a2558b96801d4d860a4c599af16510
AnwarAsry <anwareducation03@gmail.com>
2026-07-25 12:09:50 +0200

    fixed props type

--- app/components/Ingredients/IngredientsItem.tsx
+++ app/components/Ingredients/IngredientsItem.tsx
@@ -1,10 +1,17 @@
-export const IngredientsItem = ({ name, amount, unit, className }: { name: string, amount: number, unit: string, className?: string }) => {
+import type { Ingredient } from "~/models/Recipe"
+
+interface IIngredientsItemProps {
+    ingredient: Ingredient;
+    className?: string;
+}
+
+export const IngredientsItem = ({ ingredient, className }: IIngredientsItemProps) => {
     return (
         <li className={`py-2 px-3.5 flex items-center ${className || ''}`}>
             <input type="checkbox" className="w-5 h-5 mr-3 rounded text-[#4b5563]" />
             <div className="w-full flex justify-between">
-                <span className="text-base">{name}</span>
-                <span className="text-base font-medium">{amount}{unit ? ' ' + unit : ''}</span>
+                <span className="text-base">{ingredient.name}</span>
+                <span className="text-base font-medium">{ingredient.amount}{ingredient.unit ? ' ' + ingredient.unit : ''}</span>
             </div>
         </li>
     )
