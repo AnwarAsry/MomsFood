248204cc490f006b3c7af68bad17326ee529dd07
AnwarAsry <anwareducation03@gmail.com>
2026-07-26 12:15:47 +0200

    added classname prop

--- app/components/Inputs/SelectInput.tsx
+++ app/components/Inputs/SelectInput.tsx
@@ -1,20 +1,21 @@
 interface ISelectInputProps {
     value: string | number | string[] | undefined;
+    className?: string;
     name?: string;
     onChange: (e: any) => void;
     children?: React.ReactNode
 }
 
-export const SelectInput = ({ value, name, onChange, children }: ISelectInputProps) => {
+export const SelectInput = ({ value, className, name, onChange, children }: ISelectInputProps) => {
     return (
         <select
             id={name}
             name={name}
             value={value}
             onChange={onChange}
-            className="w-full px-2 py-2 border border-black/20 rounded-md 
+            className={`${className || ''} w-full px-2 py-2 border border-black/20 rounded-md 
                                             focus:border-black outline-none 
-                                            transition-colors"
+                                            transition-colors`}
         >
             {children}
         </select>
