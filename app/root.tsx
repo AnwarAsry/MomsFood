90c57c7fee334a11719eb3d84936e26961bd33c1
AnwarAsry <anwareducation03@gmail.com>
2026-07-26 13:08:16 +0200

    error for not finding the recipe

--- app/root.tsx
+++ app/root.tsx
@@ -37,7 +37,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
-        <header className="px-6 py-4 sticky top-0 z-50 border-b border-[#e5e7eb] bg-white">
+        <header className="h-18 px-6 py-4 sticky top-0 z-50 border-b border-[#e5e7eb] bg-white">
           <div className="max-w-7xl mx-auto flex items-center justify-between">
             <div className="flex items-center space-x-8">
               <div className="text-2xl text-[#111827]">RecipeHub</div>
