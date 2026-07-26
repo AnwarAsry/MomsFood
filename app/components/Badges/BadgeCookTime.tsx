66451ccdfa71a5633dc1bf8706f135ac2978804a
AnwarAsry <anwareducation03@gmail.com>
2026-07-27 01:52:14 +0200

    Simplified full condition and brand colors

--- app/components/Badges/BadgeCookTime.tsx
+++ app/components/Badges/BadgeCookTime.tsx
@@ -1,20 +1,10 @@
 import { FaFire } from "react-icons/fa6";
 
 export const BadgeCookTime = ({ cookTime, full }: { cookTime: string, full?: boolean }) => {
-
-    if (full) {
-        return (
-            <div className="flex items-center gap-1">
-                <FaFire />
-                <span>Cook time: {cookTime} min</span>
-            </div>
-        );
-    }
-
     return (
-        <div className="flex items-center gap-1">
+        <div className="flex items-center gap-1 text-brand">
             <FaFire />
-            <span>{cookTime} min</span>
+            <span className="text-gray-600">{full ? `Cook time: ${cookTime} min` : `${cookTime}`} min</span>
         </div>
     );
 }
