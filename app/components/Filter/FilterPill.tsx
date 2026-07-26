61ec9ea8a305b9e77919ff99938c2a30b5999e2c
AnwarAsry <anwareducation03@gmail.com>
2026-07-27 01:52:40 +0200

    More brand colors

--- app/components/Filter/FilterPill.tsx
+++ app/components/Filter/FilterPill.tsx
@@ -9,8 +9,8 @@ export const FilterPill = ({ text, active, onClick }: FilterPillProps) => {
         <span
             className={`
                 ${active ?
-                    "bg-[#4b5563] hover:bg-[#374151] text-white" :
-                    "border border-[#d1d5db] hover:bg-neutral-50 bg-white text-[#374151]"}
+                    "bg-brand hover:bg-brand-dark text-white" :
+                    "border border-[#d1d5db] hover:bg-brand-light bg-white text-[#374151]"}
                 px-4 py-2 rounded-xl
                 text-sm
                 cursor-pointer
--- app/components/StepCount.tsx
+++ app/components/StepCount.tsx
@@ -1,5 +1,5 @@
 export const StepCount = ({ step }: { step: number }) => {
     return (
-        <span className="min-w-8 w-8 h-8 mt-px shrink-0 flex items-center justify-center rounded-full font-medium bg-[#4b5563] text-white">{step}</span>
+        <span className="min-w-8 w-8 h-8 mt-px shrink-0 flex items-center justify-center rounded-full font-medium bg-brand-light text-brand-dark">{step}</span>
     );
 }
